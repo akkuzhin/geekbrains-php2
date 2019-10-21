a1ada2ba40107b6dae1f28229003b428d249ed8e
nurgiz akkuzhin <akkuzhin.n@gmail.com>
2019-10-22 02:07:11 +0300

    Форма добавления новости на сайт(Controller-Model-Data)

--- www/lesson1/add.php
+++ www/lesson1/add.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nurgiz
- * Date: 21.10.2019
- * Time: 23:56
- */
+
+require __DIR__ . '/models/news.php';
+
+if (!empty($_POST)) {
+    $data = [];
+    if (!empty($_POST['title'])) {
+        $data['title'] = $_POST['title'];
+    }
+    if (!empty($_POST['text'])) {
+        $data['text'] = $_POST['text'];
+    }
+    if (!empty($_POST['author'])) {
+        $data['author'] = $_POST['author'];
+    }
+
+
+    if (isset($data['title']) && isset($data['text']) && isset($data['author'])) {
+
+        News_insert($data);
+        header('Location: /lesson1/index.php');
+        die;
+    }
+}
+
+include __DIR__ . '/views/form.html';
