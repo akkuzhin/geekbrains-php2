d61e8eaf053cd23395fd6a4a26598e53f2f1bb52
nurgiz akkuzhin <akkuzhin.n@gmail.com>
2019-10-21 23:51:43 +0300

    Функции

--- www/lesson1/functions/functions.php
+++ www/lesson1/functions/functions.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nurgiz
- * Date: 21.10.2019
- * Time: 23:43
- */
+//Устанавливает новое соединение с сервером MySQL
+function sql_connect()
+{
+    return mysqli_connect('localhost', 'root', '', 'test');
+}
+
+function sql_query($sql)
+{
+    $link = sql_connect();
+    $res = mysqli_query($link, $sql);
+    $ret = [];
+    while (null !== $row = mysqli_fetch_assoc($res)) {
+        $ret[] = $row;
+    }
+    return $ret;
+}
+
+function sql_execute($sql)
+{
+    $link = sql_connect();
+    $res = mysqli_query($link, $sql);
+    if ($res) {
+        return true;
+    } else {
+        return false;
+    }
+}
