8fc38a21ff573911f1e2fd585d2d46d075d8d4cd
nurgiz akkuzhin <akkuzhin.n@gmail.com>
2019-10-22 02:02:34 +0300

    Модель для работы с данными

--- www/lesson1/models/news.php
+++ www/lesson1/models/news.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nurgiz
- * Date: 22.10.2019
- * Time: 0:06
- */
+require __DIR__ . '/../functions/functions.php';
+
+//*** Возвращает все новости с базы данных
+function News_getAll()
+{
+    $sql = 'SELECT id,date,title,author FROM news ORDER BY date DESC';
+    return sql_query($sql);
+
+}
+
+//*** Всавляет новые строки в существующую таблицу
+function News_insert($data)
+{
+    $sql = "INSERT INTO news (title, text, author)
+            VALUES ('" . $data['title'] ."', '" . $data['text'] ."', '" . $data['author'] ."')";
+    sql_exec($sql);
+}
+
+function ArticleId($id)
+{
+    $sql = "SELECT * FROM news WHERE id=" . $id;
+    return sql_query($sql);
+}
