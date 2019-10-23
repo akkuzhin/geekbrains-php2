271e7c24dc90e3da862007e16fc55a6308ad1863
nurgiz akkuzhin <akkuzhin.n@gmail.com>
2019-10-23 23:55:43 +0300

    класс, работающий с базой данных

--- www/lesson2/classes/MySQL.php
+++ www/lesson2/classes/MySQL.php
@@ -2,5 +2,31 @@
 
 class MySQL
 {
+    protected $link;
+    protected $data = [];
 
+    public function __construct()
+    {
+        $config = include __DIR__ . '/../config.php';
+        $this->link = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
+    }
+
+    public function sql_query($sql)
+    {
+        $res = mysqli_query($this->link, $sql);
+        while(null !== $row = mysqli_fetch_assoc($res)) {
+            $this->data[] = $row;
+        }
+        return $this->data;
+    }
+
+    public function sql_execute($sql)
+    {
+        $res = mysqli_query($this->link, $sql);
+        if ($res === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
